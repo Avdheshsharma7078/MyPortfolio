daa797affce447e97cc8fdcc9a3ce924be1ef093
Avdhesh Sharma <110733073+Avdheshsharma7078@users.noreply.github.com>
2024-03-26 14:35:17 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import Project4 from "./assets/project4.png";
 
 import Github from "./assets/github.png";
 import LinkedIn from "./assets/linkedin.svg";
-import Email from "./assets/email.png";
+
 import ArrowDown from "./assets/arrow-down.svg";
 import React, { useEffect, useState } from "react";
 
@@ -15,23 +15,28 @@ function App() {
   const [scrolling, setScrolling] = useState(false);
 
   const onPageScroll = () => {
-    if(window.pageYOffset > 200) {
-      setScrolling(true)
+    if (window.pageYOffset > 200) {
+      setScrolling(true);
     } else {
       setScrolling(false);
     }
-  }
+  };
 
   useEffect(() => {
-    window.addEventListener("scroll", onPageScroll)
-    return() => {
-      window.removeEventListener("scroll", onPageScroll)
-    }
-  }, [])
+    window.addEventListener("scroll", onPageScroll);
+    return () => {
+      window.removeEventListener("scroll", onPageScroll);
+    };
+  }, []);
 
   return (
     <div className="max-w-4xl m-auto relative">
-      <header className={`${scrolling ? 'border-b border-gray-900' : ''}  fixed left-0 right-0 top-0 z-20`} id="home">
+      <header
+        className={`${
+          scrolling ? "border-b border-gray-900" : ""
+        }  fixed left-0 right-0 top-0 z-20`}
+        id="home"
+      >
         <div className="container m-auto px-4 py-6 max-w-4xl bg-black">
           <div className="flex flex-col gap-4 sm:flex-row justify-between items-center">
             <div>
@@ -40,17 +45,26 @@ function App() {
             <div>
               <ul className="flex gap-4">
                 <li>
-                  <a href="#projects" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a
+                    href="#projects"
+                    className="text-gray-400 hover:text-white cursor-pointer"
+                  >
                     Projects
                   </a>
                 </li>
                 <li>
-                  <a href="#technologies" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a
+                    href="#technologies"
+                    className="text-gray-400 hover:text-white cursor-pointer"
+                  >
                     Technologies
                   </a>
                 </li>
                 <li>
-                  <a href="#aboutme" className="text-gray-400 hover:text-white cursor-pointer">
+                  <a
+                    href="#aboutme"
+                    className="text-gray-400 hover:text-white cursor-pointer"
+                  >
                     About me
                   </a>
                 </li>
@@ -66,21 +80,24 @@ function App() {
             <div>
               <h2 className="font-bold text-4xl">Hello, I'm Avdhesh Sharma,</h2>
               <div>
-                <h2 className="font-bold text-4xl mt-1 gradiant-text">Web developer</h2>
+                <h2 className="font-bold text-4xl mt-1 gradiant-text">
+                  Web developer
+                </h2>
               </div>
-              <div >
-        
-                  
-                < a href="https://drive.google.com/file/d/14sPKaF3l5vJBH81w3VK9w_ReHDH8WFy6/view?usp=drive_link">
-                <button className="px-8 shadow-gray-500 shadow-md py-5 mt-5 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700" >
-                  See resume
-                </button>
+              <div>
+                <a href="https://drive.google.com/file/d/14sPKaF3l5vJBH81w3VK9w_ReHDH8WFy6/view?usp=drive_link">
+                  <button className="px-8 shadow-gray-500 shadow-md py-5 mt-5 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700">
+                    See resume
+                  </button>
                 </a>
               </div>
             </div>
             <div className="relative">
               <div className="after:bg-[url('./large-long.png')] after:bg-contain after:block after:bg-no-repeat after:w-[420px] after:h-[320px] after:absolute after:top-0 after:-left-20 sm:after:-left-40 before:bg-[url('./small.png')] before:bg-contain before:block before:bg-no-repeat before:w-[220px] before:h-[220px] before:absolute before:bottom-0 before:-right-10">
-                <img src={Porfile} className="relative z-10 w-[280px] m-auto sm:w-[600px]" />
+                <img
+                  src={Porfile}
+                  className="relative z-10 w-[280px] m-auto sm:w-[600px]"
+                />
               </div>
             </div>
           </div>
@@ -92,51 +109,52 @@ function App() {
             <div className="flex flex-col sm:flex-row gap-10 mt-11">
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project1} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">
-                 Grabby_Food
-                </h3>
+                <h3 className="text-2xl font-semibold mt-8">Ecommerce_App</h3>
                 <p className="text-gray-400 text-sm mt-2">
-                A web application which has features of login, signup,
-selecting food from the menu and checking out with
-the food selected. Based on Reactjs, Express, MongoDB
-and Node.js.
+                  Full-stack ecommerce application built with MERN stack Buyers
+                  register and browse the marketplace while interacting with
+                  products across different categories. Admins control and
+                  manage the marketplace items and customer acounts.
                 </p>
-        
+
                 <div className="flex mt-12 gap-2">
-                
-                  <a href="https://grabbyfood-9d83f.firebaseapp.com/"  className=" btn flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700 text-center">
+                  <a
+                    href="https://mern-ecommerce-1-0cbw.onrender.com/"
+                    className=" btn flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700 text-center"
+                  >
                     Live preview
                   </a>
-               
-                 
-                  <a  href="https://github.com/Avdheshsharma7078/grabbyfood/tree/master" className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 text-center">
+
+                  <a
+                    href="https://github.com/Avdheshsharma7078/mern_ecommerce"
+                    className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 text-center"
+                  >
                     Checkout github
-                
                   </a>
                 </div>
               </div>
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project2} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">
-                 OYO_Clone
-                </h3>
+                <h3 className="text-2xl font-semibold mt-8">OYO_Clone</h3>
                 <p className="text-gray-400 text-sm mt-2">
-                A web application which has features of login, signup,
-selecting food from the menu and checking out with
-the food selected. Based on Nextjs,MongoDB
-.
+                  A web application which has features of login, signup,
+                  selecting food from the menu and checking out with the food
+                  selected. Based on Nextjs,MongoDB .
                 </p>
-        
+
                 <div className="flex mt-12 gap-2">
-                
-                  <a href="https://oyo-clone-ad499.web.app/"  className=" btn flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700 text-center">
+                  <a
+                    href="https://oyo-clone-ad499.web.app/"
+                    className=" btn flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700 text-center"
+                  >
                     Live preview
                   </a>
-               
-                 
-                  <a  href="https://github.com/Avdheshsharma7078/OYO_Clone/tree/master" className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 text-center">
+
+                  <a
+                    href="https://github.com/Avdheshsharma7078/OYO_Clone/tree/master"
+                    className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 text-center"
+                  >
                     Checkout github
-                
                   </a>
                 </div>
               </div>
@@ -144,45 +162,48 @@ the food selected. Based on Nextjs,MongoDB
             <div className="flex flex-col sm:flex-row  gap-10 mt-11">
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project3} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">
-                  My_Portfolio
-                </h3>
-                <p className="text-gray-400 text-sm mt-2">
-                 Using Tailwind CSS
-                </p>
+                <h3 className="text-2xl font-semibold mt-8">My_Portfolio</h3>
+                <p className="text-gray-400 text-sm mt-2">Using Tailwind CSS</p>
                 <div className="flex gap-4 mt-12">
-                  <a href="https://myportfolio-23980.web.app/" className="btn flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700 text-center">
+                  <a
+                    href="https://myportfolio-23980.web.app/"
+                    className="btn flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700 text-center"
+                  >
                     Live preview
                   </a>
-                  <a className="btn flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 text-center">
+                  <a
+                    href="https://github.com/Avdheshsharma7078/MyPortfolio/tree/master"
+                    className="btn flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 text-center"
+                  >
                     Checkout github
                   </a>
                 </div>
               </div>
               <div className="border border-gray-500 rounded-md p-5 flex-1">
                 <img src={Project4} className="w-full h-auto" />
-                <h3 className="text-2xl font-semibold mt-8">
-                 Netflix_Clone
-                </h3>
+                <h3 className="text-2xl font-semibold mt-8">Netflix_Clone</h3>
                 <p className="text-gray-400 text-sm mt-2">
-                This is a clone of Netflix website built using React.JS
-as a Front-end & Firebase as Back-end. It's not a replica,
-and it doesn't have all the features of Netflix website.
-it's a similar version of Netflix with my own design touch,
-showing my abilities in React.JS to build something advanced
-like Netflix.
+                  This is a clone of Netflix website built using React.JS as a
+                  Front-end & Firebase as Back-end. It's not a replica, and it
+                  doesn't have all the features of Netflix website. it's a
+                  similar version of Netflix with my own design touch, showing
+                  my abilities in React.JS to build something advanced like
+                  Netflix.
                 </p>
-        
+
                 <div className="flex mt-12 gap-2">
-                
-                  <a href="https://netflix-clone-758bd.web.app/"  className=" btn flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700 text-center">
+                  <a
+                    href="https://netflix-clone-758bd.web.app/"
+                    className=" btn flex-1 text-sm py-3 bg-gradient-to-t from-blue-500 rounded-full to-cyan-500 hover:from-blue-700 hover:to-cyan-700 text-center"
+                  >
                     Live preview
                   </a>
-               
-                 
-                  <a  href="https://github.com/Avdheshsharma7078/Netflix_Clone/tree/master" className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 text-center">
+
+                  <a
+                    href="https://github.com/Avdheshsharma7078/Netflix_Clone/tree/master"
+                    className="flex-1 text-sm py-3 border rounded-full hover:border-blue-500 hover:text-blue-500 text-center"
+                  >
                     Checkout github
-                
                   </a>
                 </div>
               </div>
@@ -210,16 +231,16 @@ like Netflix.
               </div>
               <div className="mt-8">
                 <div className="flex justify-between items-center">
-                  <h2 className="font-semibold">
-                    JavaScript
-                  </h2>
+                  <h2 className="font-semibold">JavaScript</h2>
                   <p className="text-gray-500">Moderate</p>
                 </div>
                 <span className="w-[70%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
               </div>
               <div className="mt-8">
                 <div className="flex justify-between items-center">
-                  <h2 className="font-semibold">Database like Mysql ,MongoDB</h2>
+                  <h2 className="font-semibold">
+                    Database like Mysql ,MongoDB
+                  </h2>
                   <p className="text-gray-500">Advanced</p>
                 </div>
                 <span className="w-[75%] h-2 mt-2 bg-gradient-to-t from-blue-500 to-cyan-500 block rounded-md" />
@@ -260,7 +281,7 @@ like Netflix.
                   Git
                 </p>
               </div>
-             
+
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
                   Teamwork
@@ -280,7 +301,7 @@ like Netflix.
               </div>
               <div>
                 <p className="font-bold before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:block before:rounded-full before:mt-1 before:-left-6 before:absolute relative left-5">
-               Languages Like  Hindi & english
+                  Languages Like Hindi & english
                 </p>
               </div>
               <div>
@@ -298,22 +319,25 @@ like Netflix.
               <div className="pl-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
                 <h3 className="absolute left-0 text-lg font-semibold">2018</h3>
                 <p>
-                 
- 10th: Crucial milestone, covering core subjects, assessments for academic proficiency, marking transition towards higher education.
+                  10th: Crucial milestone, covering core subjects, assessments
+                  for academic proficiency, marking transition towards higher
+                  education.
                 </p>
               </div>
               <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
                 <h3 className="absolute left-0 text-lg font-semibold">2020</h3>
                 <p>
-                 12th: Big test after a lot of studying, showing what you know, getting into college, and planning for the future..
+                  12th: Big test after a lot of studying, showing what you know,
+                  getting into college, and planning for the future..
                 </p>
               </div>
-             
+
               <div className="pl-24 mt-24 relative before:w-4 before:h-4 before:bg-gradient-to-t before:from-blue-500 before:to-cyan-500 before:absolute before:rounded-full before:left-[58px]">
                 <h3 className="absolute left-0 text-lg font-semibold">2024</h3>
                 <p>
-                  
-By 2024, I'll graduate with a B.Tech in Computer Science, equipped for exciting opportunities in technology and innovation.
+                  By 2024, I'll graduate with a B.Tech in Computer Science,
+                  equipped for exciting opportunities in technology and
+                  innovation.
                 </p>
               </div>
             </div>
@@ -321,12 +345,12 @@ By 2024, I'll graduate with a B.Tech in Computer Science, equipped for exciting 
         </section>
       </main>
       <footer>
-      <div>
-                  <h2  className="text-blue-400 border-l-white">Contact Details =</h2>
-                  9368140934
-                  <br></br>
-                  avdheshsharma7078@gmail.com
-                </div>
+        <div>
+          <h2 className="text-blue-400 border-l-white">Contact Details =</h2>
+          9368140934
+          <br></br>
+          avdheshsharma7078@gmail.com
+        </div>
         <div className="container m-auto flex justify-between px-4 py-6">
           <div>
             <p className="text-gray-300 text-sm">Copyright @ 2023</p>
@@ -335,31 +359,29 @@ By 2024, I'll graduate with a B.Tech in Computer Science, equipped for exciting 
             <ul className="flex gap-4">
               <li>
                 <a href="https://github.com/Avdheshsharma7078">
-                  <img src={Github} className="w-5"  />
+                  <img src={Github} className="w-5" />
                 </a>
               </li>
               <li>
                 <a href="https://www.linkedin.com/in/avdhesh-sharma-8a2b23227/">
                   <img src={LinkedIn} className="w-5" />
                 </a>
-               
               </li>
-              <li>
-               
-              </li>
+              <li></li>
             </ul>
           </div>
         </div>
       </footer>
-      {
-        scrolling && (
-          <button className="fixed block right-8 bottom-0 w-24" onClick={() => {
-            window.scrollTo(0,0);
-          }}>
-            <img src={ArrowDown} />
-          </button>
-        )
-      }
+      {scrolling && (
+        <button
+          className="fixed block right-8 bottom-0 w-24"
+          onClick={() => {
+            window.scrollTo(0, 0);
+          }}
+        >
+          <img src={ArrowDown} />
+        </button>
+      )}
     </div>
   );
 }
